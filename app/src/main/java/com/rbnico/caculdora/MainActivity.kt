a9867d9760c2d4c49975fc552f42111d0e99ed59
Rubén Muñoz <rubenmunoz.alu@iespacomolla.es>
2021-12-14 13:03:04 +0100

    Calculadora terminada

--- app/src/main/java/com/rbnico/caculdora/MainActivity.kt
+++ app/src/main/java/com/rbnico/caculdora/MainActivity.kt
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener
 {
-
 
     var n1 = 0.0
     var n2 = 0.0
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener
     var new = true
     var equal: Boolean = false
 
-    @Throws(Exception::class)
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
@@ -50,7 +49,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener
 
     override fun onClick(v: View?)
     {
-        var n = (v as Button).text as String
+
+        var n: String
+        try
+        {
+            n = (v as Button).text as String
+        } catch (e: Exception){
+            n=""
+        }
 
         when (v)
         {
@@ -81,135 +87,257 @@ class MainActivity : AppCompatActivity(), View.OnClickListener
         }
     }
 
-    private fun masMenos()
+    private fun masMenos() // CAMBIO DE SIGNO
     {
-        if(screen.text.toString() != "0")
+        try
         {
-            if(!screen.text.toString().startsWith("-"))
+            if(screen.text.toString() != "0") //SOLO LO CAMBIA SI NO ES CERO
             {
-                screen.text = screen.text.toString().replace("+", "")
-                screen.text = "-" + screen.text.toString()
+                if(!screen.text.toString().startsWith("-")) //SI ES NEGATIVO LO ASIGNA PONE EN POSITIVO
+                {
+                    screen.text = screen.text.toString().replace("+", "")
+                    screen.text = "-" + screen.text.toString()
+                }
+                else //SI ES POSITIVO LO ASIGNA PONE EN NEGATIVO
+                {
+                    screen.text = screen.text.toString().replace("-", "")
+                    screen.text = "+" + screen.text.toString()
+                }
             }
-            else
-            {
-                screen.text = screen.text.toString().replace("-", "")
-                screen.text = "+" + screen.text.toString()
-            }
+        } catch(e: Exception)
+        {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
         }
 
     }
 
     private fun coma(n: String)
     {
-
-        if(!(screen.text.toString().contains(".")))
-            if(screen.text.toString()=="0")
-                screenWriter("0.")
-            else
-                screenWriter(n)
+        try
+        {
+            if(!(screen.text.toString().contains("."))) // SÓLO SI NO TIENE PUNTO
+                if(screen.text.toString()=="0")
+                    screenWriter("0.")
+                else
+                    screenWriter(n)
+        } catch(e: Exception)
+        {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
+        }
     }
 
     private fun porcentaje()
     {
-        operationPressed("/")
-        numberPressed("100")
-        equalPressed()
+        try
+        {
+            when(op)
+            {
+                "x" ->
+                {
+                    screen.text = (screen.text.toString().toDouble()/100).toString()
+                }
+                "/" -> equalPressed()
+                null ->
+                {
+                    screen.text = (screen.text.toString().toDouble()/100).toString()
+                }
+                "+" ->
+                {
+                    screen.text = (screen.text.toString().toDouble() * n1 / 100).toString()
+                }
+                "-" ->
+                {
+                    screen.text = (screen.text.toString().toDouble() * n1 / 100).toString()
+                }
+            }
+        } catch(e: Exception)
+        {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
+        }
     }
 
     private fun numberPressed(n: String)
     {
-        if(new)
+        try
         {
-            screen.text = ""
-        }
+            if(new)
+            {
+                screen.text = ""
+            }
 
-        new = false
-        screenWriter(n)
+            new = false
+            screenWriter(n)
+
+        } catch(e: Exception)
+        {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
+        }
     }
 
     private fun clearScreen()
     {
-        n1 = 0.0
-        n2 = 0.0
-        op = null
-        screen.text = "0"
+        try
+        {
+            n1 = 0.0
+            n2 = 0.0
+            op = null
+            screen.text = "0"
+            screenSymbol.text = ""
+        } catch(e: Exception)
+        {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
+        }
     }
 
     private fun borrar()
     {
-        if(screen.text.toString().length == 2 && (screen.text.toString().startsWith("+") || screen.text.toString().startsWith("-")))
+        try
         {
-            screen.text = screen.text.toString().dropLast(2)
-        }else if (screen.text.toString().length == 0 || (screen.text.toString().length == 2 && screen.text.toString().contains("."))) {
-            screen.text = "0"
-        }else
-            screen.text = screen.text.toString().dropLast(1)
+            //SI TIENE UN DÍGITO Y EMPIEZA CON SIGNO
+            if(screen.text.toString().length == 2 && (screen.text.toString().startsWith("+") || screen.text.toString().startsWith("-")))
+            {
+                screen.text = screen.text.toString().dropLast(2)
 
-        if(screen.text.toString().length==0)
-            screen.text = "0"
+
+            //SI ESTÁ VACÍO O SOLO LE QUEDA UN NÚMERO Y EL PUNTO DECIMAL
+            }else if (screen.text.toString().length == 0 || (screen.text.toString().length == 2 && screen.text.toString().contains("."))) {
+                screen.text = "0"
+
+
+            //SI ESTÁ EN NOTACIÓN CIENTÍFICA
+            } else if(screen.text.toString().contains('E'))
+            {
+                operationPressed("/")
+            } else
+            {
+                screen.text = screen.text.toString().dropLast(1)
+            }
+
+
+
+            //SI ESTÁ VACÍO, LO PONE A CERO
+            if(screen.text.toString().length==0)
+                screen.text = "0"
+        } catch(e: Exception)
+        {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
+        }
 
     }
 
     private fun operationPressed(operation: String)
     {
-        equalPressed()
-        new = true
-        this.op = operation
-        n1 = screen.text.toString().toDouble()
+        try
+        {
+            equalPressed()
+            new = true
+            this.op = operation
+            n1 = screen.text.toString().toDouble()
+            screenSymbol.text  = op
+        } catch(e: Exception)
+        {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
+        }
     }
 
     private fun screenWriter(n: String)
     {
-        var result = if(screen.text == "0" && n != ",")
-            n
-        else
-            "${screen.text}$n"
-
-        while(result.contains(".") && result.endsWith("0",true))
+        try
         {
-            result.dropLast(1)
+            var result = if(screen.text == "0" && n != ".")
+                n
+            else
+                "${screen.text}$n"
+
+            screen.text = result
+        } catch(e: Exception)
+        {
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
         }
 
 
-        screen.text = result
-
-
-    }
-
-    private fun checkOperation()
-    {
-        if(op == null)
-            n1 = screen.text.toString().toDouble()
-        else
-            n2 = screen.text.toString().toDouble()
     }
 
     private fun equalPressed()
     {
-        n2 = screen.text.toString().toDouble()
 
-        if (n2 == 0.0 && op == "/") {
-            screen.text = "0"
-            return
-        }
-        val resultado =
-            when(op)
+
+        var resultadoExponencial: Double = 0.0
+        var exponente = 0
+        var stringResult: String = ""
+        var resultado: Double
+
+
+        try
+        {
+            screenSymbol.text = ""
+            if(screen.text.toString().contains('E'))
             {
-                "+" -> n1+n2
-                "-" -> n1-n2
-                "/" -> n1/n2
-                "X" -> n1*n2
-                else -> screen.text.toString().toDouble()
+                exponente = Integer.valueOf(screen.text.toString()[screen.text.toString().length-1].toString())
+                n2 = screen.text.toString().dropLast(2).toDouble()
+                for(i in 1..exponente)
+                {
+                    n2 = n2*10
+                }
+            }else
+            {
+                n2 = screen.text.toString().toDouble()
             }
-        op = null
-        n1 = resultado.toDouble()
+            exponente = 0
 
-        screen.text = if(resultado.toString().endsWith(".0"))
+            if (n2 == 0.0 && op == "/") {
+                screen.text = "0"
+                return
+            }
+            resultado =
+                when(op)
+                {
+                    "+" -> n1+n2
+                    "-" -> n1-n2
+                    "/" -> n1/n2
+                    "x" -> n1*n2
+                    else -> screen.text.toString().toDouble()
+                }
+
+            resultadoExponencial = resultado
+
+            if(resultadoExponencial > 1000000)
+            {
+                while(resultadoExponencial > 10)
+                {
+                    resultadoExponencial = resultadoExponencial / 10
+                    exponente += 1
+                }
+            }
+
+            op = null
+            n1 = resultado
+
+            if(exponente > 1)
+            {
+                stringResult = resultadoExponencial.toString() + "E" + exponente
+            } else
+            {
+                stringResult = resultado.toString()
+            }
+
+            if(stringResult.contains('E'))
+            {
+                screen.text = stringResult
+            }else
+            {
+                if(resultado.toString().endsWith(".0"))
+                {
+                    screen.text = resultado.toString().replace(".0", "")
+                } else
+                {
+                    screen.text = "%.2f".format(resultado)
+                }
+            }
+        } catch(e: Exception)
         {
-            resultado.toString().replace(".0", "")
-        } else
-        {
-            "%.2f".format(resultado)
+            Toast.makeText(this, e.message, Toast.LENGTH_SHORT)
         }
+        new = true
     }
 }
